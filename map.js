e0c551db8c72ed3362c3d27db4a963391b847682
matusmlady <96914042+matusmlady@users.noreply.github.com>
2022-02-19 20:53:19 +0000

    elements working

--- map.js
+++ map.js
@@ -1,12 +1,15 @@
 //deastinne cisla v pomeroch nefunguju
 
-//podmienka aby sa diagonalne veci nekrizovali
-//okolie pre elementy horizontalne vertikalne a diagonalne
 //grafika elementom
 //features
+//statistiky live kvoli dedinam
+//?shhapes ako vlastnost elementu
+//hrady ako features
 
-columns = 50
-rows = 50
+//?spolocne vlastnosti (banovanie moze byt jednostranne ak nie je zadefinovane inak)
+
+columns = 15
+rows = 15
 dimension = 20
 
 polia = []
@@ -38,22 +41,21 @@ data.element.zoznam = []
 data.feature.zoznam = []
 
 data.instructions = new Object()
-//data.instructions.zoznam = [["vlavo"], ["vpravo"], ["hore"], ["dole"], ["vlavo", "hore"], ["vlavo", "dole"], ["vpravo", "hore"], ["vpravo", "dole"]]
 data.instructions["vlavo"] = - 1
 data.instructions["vpravo"] = 1
 data.instructions["hore"] = - columns
 data.instructions["dole"] = columns
 
-
-
-data.yellow = new farba("podlazie", "yellow", 25, 1, [{action: 0, colors: ["white"], okruhy: [1, 2, 3]}, {action: 50, colors: ["yellow"], okruhy: [1, 2, 3]}])
+data.yellow = new farba("podlazie", "yellow", 25, 1, [{action: 0, colors: ["white"], okruhy: [1, 2, 3]}, {action: 50, colors: ["yellow"], okruhy: [1, 2, 3]}, {action: 0, colors: ["burlywood"], okruhy: [0]}])
 data.white = new farba("podlazie", "white", 50, 1, [{action: 0, colors: ["yellow", "gold"], okruhy: [1, 2, 3]}, {action: 50, colors: ["white"], okruhy: [1, 2, 3]}])
 data.green = new farba("podlazie", "green", 250)
-data.blue = new farba("podlazie", "blue", 5, 1, [{action: 80, colors: ["blue"], okruhy: [1, 2, 3]}])
-data.gold = new farba("podlazie", "gold", 25, 1, [{action: 0, colors: ["white"], okruhy: [1, 2, 3]}, {action: 40, colors: ["gold"], okruhy: [1, 2, 3]}, {action: 10, colors: ["blue"], okruhy: [1, 2, 3]}])
+data.blue = new farba("podlazie", "blue", 5, 1, [{action: 80, colors: ["blue"], okruhy: [1, 2, 3]}, {action: 0, colors: ["brown", "aqua", "burlywood"], okruhy: [0]}])
+data.gold = new farba("podlazie", "gold", 25, 1, [{action: 0, colors: ["white"], okruhy: [1, 2, 3]}, {action: 40, colors: ["gold"], okruhy: [1, 2, 3]}, {action: 10, colors: ["blue"], okruhy: [1, 2, 3]}, {action: 0, colors: ["burlywood"], okruhy: [0]}])
 
-data.brown = new farba("element", "brown", 1, 3)
-data.none = new farba("element", "none", 10, 1)
+data.brown = new farba("element", "brown", 5, 3, [{action: 0, colors: ["blue"], okruhy: [0]}])
+data.burlywood = new farba("element", "burlywood", 5, 2, [{action: 0, colors: ["blue", "yellow", "gold"], okruhy: [0]}])
+data.aqua = new farba("element", "aqua", 5, 2, [{action: 0, colors: ["blue"], okruhy: [0]}])
+data.none = new farba("element", "none", 100, 1)
 
 data.ziadne = new farba("feature", "ziadne", 1, 1)
 
@@ -96,8 +98,6 @@ class pole {
     
   }
   
-  
-  
   vyberMain(arg){
     if (this[arg].chosen != ""){
       return
@@ -112,12 +112,32 @@ class pole {
               break
       }
     }
-    while (this.smer(data.instructions.zoznam, this[arg].chosen) != 1)
+    while (!this.smer(this[arg].chosen))
      
     for (let x = 0; x < data[this[arg].chosen].properties.length; x++){
       let todo = []
+
       for (let y = 0; y < data[this[arg].chosen].properties[x].okruhy.length; y++){
-        todo = this.stvorec(todo, data[this[arg].chosen].properties[x].okruhy[y])
+        let todoMinus = []
+        let todoPlus = []
+        //todo = this.stvorec(todo, data[this[arg].chosen].properties[x].okruhy[y])
+        for (let z = 0; z < this[arg].rozmery.length; z++){
+          //console.log("robim todo plus")
+          todoPlus = polia[this[arg].rozmery[z]].stvorec(todoPlus, data[this[arg].chosen].properties[x].okruhy[y])
+          
+        }
+        for (let z = 0; z < this[arg].rozmery.length; z++){
+          //console.log("robim todo minus")
+          //console.log("okruh je velky pred odoslanim: ", data[this[arg].chosen].properties[x].okruhy[y])
+          todoMinus = polia[this[arg].rozmery[z]].stvorec(todoMinus, data[this[arg].chosen].properties[x].okruhy[y] - 1)
+          
+        }
+        
+        
+        todo = [...todo, ...todoPlus.filter(x => !todoMinus.includes(x))]
+        
+        /*console.log(todo, todoMinus)*/
+        //this.stvorec(todo, data[this[arg].chosen].properties[x].okruhy[y])
       }
       this.uprav(todo, data[this[arg].chosen].properties[x].action, data[this[arg].chosen].properties[x].colors)
       
@@ -125,53 +145,49 @@ class pole {
       
   }
 
-  smer(arg, element){
-    let smery = [["vlavo"], ["vpravo"], ["hore"], ["dole"], ["vlavo", "hore"], ["vlavo", "dole"], ["vpravo", "hore"], ["vpravo", "dole"]]
-    let ktory = 0
-    for (let x = 0; x < 8; x++){
-      ktory = Math.floor(Math.random() * smery.length)
-      if (this.check(smery[ktory], element)){
-        return 1
-      } else {
-        smery.splice(ktory, 1)
-      }
-      
+  stvorec(todo, okruh){
+    if (okruh == 0){
+      //console.log("returning from stvorec pre okruh 0")
+      //console.log("stvorec vracia", [this.index])
+      return [...todo, this.index]
     }
-    this.farby.positive[element] = 0
-    return 0
+    if (okruh == - 1){
+          //console.log("returning from stvorec pre okruh - 1")
+      return todo
+    }
+    //console.log("todo arg:", todo, "okruh arg:", okruh)
+    let todoOkolie = todo
+    todoOkolie = this.hrana(todoOkolie, okruh, 1, this.hore, this.index - okruh * columns, "vlavo", okruh, this.vlavo, "vpravo", this.vpravo)
+    todoOkolie = this.hrana(todoOkolie, okruh, 1, this.dole, this.index + okruh * columns, "vlavo", okruh, this.vlavo, "vpravo", this.vpravo)
+    todoOkolie = this.hrana(todoOkolie, okruh, columns, this.vlavo, this.index - okruh, "hore", okruh * columns - columns, this.hore * columns, "dole", this.dole * columns)
+    todoOkolie = this.hrana(todoOkolie, okruh, columns, this.vpravo, this.index + okruh, "hore", okruh * columns - columns, this.hore * columns, "dole", this.dole * columns)
+    //console.log("stvorec vracia:", todoOkolie)
+    return todoOkolie
+
   }
-
-  check(arg, element){
-    let pokyn = arg
-    for (let x = 0; x < pokyn.length; x++){
-      if (this[pokyn[x]] < data[element].sirka - 1){
-        return 0
+  
+  hrana(todoOkolie = [], okruh = 0, increase = 0, miesto = 0, bod = 0, smer1 = "", udaj0 = 0, udaj1 = 0, smer2 = "", udaj2 = 0){
+    const todoSuradnice = todoOkolie
+    if (miesto >= okruh){
+      let x
+      if (this[smer1] >= okruh){
+        x = polia[bod].index - udaj0
+      } else {
+        x = polia[bod].index - udaj1
+      }
+      let y
+      if (this[smer2] >= okruh){
+        y = polia[bod].index + udaj0
+      } else {
+        y = polia[bod].index + udaj2
+      }
+      for ( ; x <= y; x += increase){
+        if (!todoSuradnice.includes(x)){
+          todoSuradnice.push(x)
+        }
       }
     }
-    let suradnicky = [this.index]
-    let pseudo = this.index;
-    for (let x = 0; x < data[element].sirka - 1; x++){
-      for (let x = 0; x < pokyn.length; x++){
-        pseudo += data.instructions[pokyn[x]]
-      }
-      suradnicky.push(pseudo)
-      if (polia[pseudo][data[element].typ].chosen != ""){
-        return 0
-      }
-      if (polia[pseudo].farby.banned[element] == 1){
-        return 0
-      }
-      
-    }
-    for (let x = 0; x < suradnicky.length; x++){
-      polia[suradnicky[x]][data[element].typ].chosen = element
-      polia[suradnicky[x]][data[element].typ].rozmery = []
-      for (let y = 0; y < suradnicky.length; y++){
-        polia[suradnicky[x]][data[element].typ].rozmery.push(suradnicky.length)
-      }
-
-    }
-    return 1
+    return todoSuradnice
   }
   
   
@@ -218,42 +234,63 @@ class pole {
       }
     }
   }
-
-  stvorec(todo, okruh){
-    if (okruh == 0){
-      return this.index
-    }
-    let todoOkolie = todo
-    todoOkolie = this.hrana(todoOkolie, okruh, 1, this.hore, this.index - okruh * columns, "vlavo", okruh, this.vlavo, "vpravo", this.vpravo)
-    todoOkolie = this.hrana(todoOkolie, okruh, 1, this.dole, this.index + okruh * columns, "vlavo", okruh, this.vlavo, "vpravo", this.vpravo)
-    todoOkolie = this.hrana(todoOkolie, okruh, columns, this.vlavo, this.index - okruh, "hore", okruh * columns - columns, this.hore * columns, "dole", this.dole * columns)
-    todoOkolie = this.hrana(todoOkolie, okruh, columns, this.vpravo, this.index + okruh, "hore", okruh * columns - columns, this.hore * columns, "dole", this.dole * columns)
-    return todoOkolie
-
-  }
   
-  hrana(todoOkolie = [], okruh = 0, increase = 0, miesto = 0, bod = 0, smer1 = "", udaj0 = 0, udaj1 = 0, smer2 = "", udaj2 = 0){
-    const todoSuradnice = todoOkolie
-    if (miesto >= okruh){
-      let x
-      if (this[smer1] >= okruh){
-        x = polia[bod].index - udaj0
+  smer(element){
+    let smery = [["vlavo"], ["vpravo"], ["hore"], ["dole"], ["vlavo", "hore"], ["vlavo", "dole"], ["vpravo", "hore"], ["vpravo", "dole"]]
+    let ktory = 0
+    for (let x = 0; x < 8; x++){
+      ktory = Math.floor(Math.random() * smery.length)
+      if (this.check(smery[ktory], element)){
+        return 1
       } else {
-        x = polia[bod].index - udaj1
+        smery.splice(ktory, 1)
       }
-      let y
-      if (this[smer2] >= okruh){
-        y = polia[bod].index + udaj0
-      } else {
-        y = polia[bod].index + udaj2
+      
+    }
+    this.farby.positive[element] = 0
+    return 0
+  }
+
+  check(arg, element){
+    let pokyn = arg
+    for (let x = 0; x < pokyn.length; x++){
+      if (this[pokyn[x]] < data[element].sirka - 1){
+        return 0
       }
-      for ( ; x <= y; x += increase){
-        if (todoSuradnice.indexOf(x) == - 1){
-          todoSuradnice.push(x)
+    }
+    let suradnicky = [this.index]
+    let pseudo = this.index;
+    for (let x = 0; x < data[element].sirka - 1; x++){
+      if (pokyn.length == 2){
+        let prvePoleRozmery = polia[pseudo + data.instructions[pokyn[0]]][data[element].typ].rozmery
+        let druhePoleRozmery = polia[pseudo + data.instructions[pokyn[1]]][data[element].typ].rozmery;
+        for (let y = 0; y < prvePoleRozmery.length; y++){
+          if (druhePoleRozmery.includes(prvePoleRozmery[y])){
+            return 0
+          }
         }
       }
+      for (let y = 0; y < pokyn.length; y++){
+        pseudo += data.instructions[pokyn[y]]
+      }
+      suradnicky.push(pseudo)
+      if (polia[pseudo][data[element].typ].chosen != ""){
+        return 0
+      }
+      if (polia[pseudo].farby.banned[element] == 1){
+        return 0
+      }
+      
     }
-    return todoSuradnice
+    for (let x = 0; x < suradnicky.length; x++){
+      polia[suradnicky[x]][data[element].typ].chosen = element
+      polia[suradnicky[x]][data[element].typ].rozmery = []
+      for (let y = 0; y < suradnicky.length; y++){
+        polia[suradnicky[x]][data[element].typ].rozmery.push(suradnicky[y])
+      }
+
+    }
+    return 1
   }
   
   uprav(arg = [], action = 0, colors = []){
@@ -278,6 +315,41 @@ class pole {
 }
    
 function main(){
+  function pozadia(){
+    for (r = 0; r < rows; r++){
+      for (c = 0; c < columns; c++){
+        ctx.fillStyle = polia[r*columns+c].podlazie.chosen
+        ctx.beginPath()
+        ctx.rect(c*dimension, r*dimension, dimension, dimension)
+        ctx.fill()
+        ctx.stroke()
+      }
+    }
+  }
+  
+  function elementy(){
+    for (r = 0; r < rows; r++){
+      for (c = 0; c < columns; c++){
+        if (polia[r*columns+c].element.chosen != "none"){
+          ctx.fillStyle = polia[r*columns+c].element.chosen
+          ctx.beginPath()
+          ctx.rect(c*dimension+5, r*dimension+5, dimension-10, dimension-10)
+          ctx.fill()
+          ctx.stroke()
+          
+          ctx.strokeStyle = "lime"
+          ctx.beginPath();
+          ctx.moveTo(polia[polia[r*columns+c].element.rozmery[0]].vlavo * dimension + dimension / 2, polia[polia[r*columns+c].element.rozmery[0]].hore * dimension + dimension / 2);
+          let poslednyRozmer = polia[r*columns+c].element.rozmery.length - 1
+          ctx.lineTo(polia[polia[r*columns+c].element.rozmery[poslednyRozmer]].vlavo * dimension + dimension / 2, polia[polia[r*columns+c].element.rozmery[poslednyRozmer]].hore * dimension + dimension / 2);
+          ctx.stroke();
+          ctx.strokeStyle = "black"
+        }
+
+      }
+    }
+  }
+  
   let poliaRaw = []
   for (let i = 0; i < rows * columns; i++){
     poliaRaw.push([i, "podlazie", "element", "feature"])
@@ -292,50 +364,25 @@ function main(){
       poliaRaw.splice(cislo, 1)
     }
   }
+  c = document.getElementById("myCanvas")
+  c.width = columns * dimension
+  c.height = rows * dimension
+  c.style.width = columns * dimension + 'px'
+  c.style.height = rows * dimension + 'px'
+  ctx = c.getContext("2d")
+
+  ctx.strokeStyle = "black"
+
+  pozadia()
+  elementy()
 
 }
 
 main()
 
-c = document.getElementById("myCanvas")
-c.width = columns * dimension
-c.height = rows * dimension
-c.style.width = columns * dimension + 'px'
-c.style.height = rows * dimension + 'px'
-ctx = c.getContext("2d")
-
-pozadia()
-
-function pozadia(){
-  ctx.strokeStyle = "black"
-  for (r = 0; r < rows; r++){
-    for (c = 0; c < columns; c++){
-      ctx.fillStyle = polia[r*columns+c].podlazie.chosen
-      ctx.beginPath()
-      ctx.rect(c*dimension, r*dimension, dimension, dimension)
-      ctx.fill()
-      ctx.stroke()
-    }
-  }
-}
-
-  for (r = 0; r < rows; r++){
-    for (c = 0; c < columns; c++){
-      if (polia[r*columns+c].element.chosen != "none"){
-        ctx.fillStyle = polia[r*columns+c].element.chosen
-        ctx.beginPath()
-        ctx.rect(c*dimension+5, r*dimension+5, dimension-10, dimension-10)
-        ctx.fill()
-        ctx.stroke()
-      }
-
-    }
-  }
-  
 console.log(polia,'polia')
 
 console.log(data)
-
 
 
 
