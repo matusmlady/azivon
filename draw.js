30e1229432173703990d619420e3fd026d85151d
matusmlady <96914042+matusmlady@users.noreply.github.com>
2022-05-09 12:49:18 +0000

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,10 +1,10 @@
-dimension = 40
+const dimension = 40
 
 function draw(columns, rows, dimension){
   function floorsFeatures(arg1, arg2, arg3){
     for (r = 0; r < rows; r++){
       for (c = 0; c < columns; c++){
-        if (data[tiles[r*columns+c][arg3].chosen].color != "none"){
+        if (data[tiles[r * columns + c][arg3].chosen].color != "none"){
           ctx.fillStyle = data[tiles[r*columns+c][arg3].chosen].color
           ctx.beginPath()
           ctx.moveTo(c * dimension + tiles[r*columns+c][arg3].vpravo[0] * dimension, r * dimension + tiles[r*columns+c][arg3].vpravo[1] * dimension)
@@ -27,20 +27,20 @@ function draw(columns, rows, dimension){
   
   function loot(){
     ctx.fillStyle = 'black';
-    for(var i = 0; i < data.loot.list.length && data.loot[data.loot["list"][i]].length>0; i++){
+    for(let i = 0; i < data.loot.list.length && data.loot[data.loot["list"][i]].length>0; i++){
       ctx.font = "14px Arial"
       ctx.rotate(90 * Math.PI / 180);
       ctx.fillText(data.loot.list[i],0,-columns*dimension-5-i*dimension);
       ctx.rotate(270 * Math.PI / 180);
       ctx.font = "14px Arial"
     
-      for (var x =0; x < data.loot[data.loot["list"][i]].length; x++){
+      for (let x = 0; x < data.loot[data.loot["list"][i]].length; x++){
         ctx.fillText(data.loot[data.loot["list"][i]][x],columns*dimension+i*dimension+5, Math.ceil(ctx.measureText(data.loot["list"][i]).width)+23+x*20);
       }
     }
   }
   function noLoot(){
-    for(var i = 0; i < data.loot.list.length && data.loot[data.loot["list"][i]].length>0; i++){
+    for(let i = 0; i < data.loot.list.length && data.loot[data.loot["list"][i]].length>0; i++){
         return data.loot.list.length*dimension + 10;
     }
       return 0;
@@ -48,10 +48,10 @@ function draw(columns, rows, dimension){
   c = document.getElementById("map")
   ctx = c.getContext("2d")
 
-  if(rows*dimension > data.loot.maxLoot()){
+  if (rows*dimension > data.loot.maxLoot()){
     c.height = rows * dimension
     c.style.height = rows * dimension + 'px'
-  }else{
+  } else {
     c.height =  data.loot.maxLoot()
     c.style.height = data.loot.maxLoot() + 'px'
   }
@@ -66,22 +66,9 @@ function draw(columns, rows, dimension){
   floorsFeatures(dimension/3, 3, "feature")
   loot()
   console.log(data)
-  /*
-  ctx.beginPath()
-  ctx.moveTo(c * dimension + tiles[r*columns+c].element.vpravo[0] * dimension, r * dimension + tiles[r*columns+c].element.vpravo[1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element["vpravo,dole"][0] * dimension, r * dimension + tiles[r*columns+c].element["vpravo,dole"][1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element.dole[0] * dimension, r * dimension + tiles[r*columns+c].element.dole[1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element["vlavo,dole"][0] * dimension, r * dimension + tiles[r*columns+c].element["vlavo,dole"][1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element.vlavo[0] * dimension, r * dimension + tiles[r*columns+c].element.vlavo[1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element["vlavo,hore"][0] * dimension, r * dimension + tiles[r*columns+c].element["vlavo,hore"][1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element.hore[0] * dimension, r * dimension + tiles[r*columns+c].element.hore[1] * dimension)
-  ctx.lineTo(c * dimension + tiles[r*columns+c].element["vpravo,hore"][0] * dimension, r * dimension + tiles[r*columns+c].element["vpravo,hore"][1] * dimension)
 
-  ctx.closePath()
-  ctx.fill()
-  ctx.stroke()
-  */
 }
+
 function printMap(){  
   let dataUrl = document.getElementById('map').toDataURL();  
   let windowContent = '<!DOCTYPE html>';
