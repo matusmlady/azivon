590731abe901d2ef8757d80dd163a86a2b0f0f31
matusmlady <alien92@tutanota.com>
2023-03-29 19:25:15 +0200

    the new beginning

--- js/index.js
+++
@@ -1,9 +0,0 @@
-//TODO remove this file
-const d = createDefaultDeck()
-m = generateMap(d, d.columns, d.rows)
-
-function generateMapIndexHtml(){
-  for (const l in d.loot) d.loot[l] = []
-  for (const c in d.count) d.count[c] = 0
-  m = generateMap(d, 15, 15)
-}
