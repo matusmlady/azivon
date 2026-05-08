2b7f8e8608110c30a735252e3cf05dad598f767c
Matúš Mladý <alien92@tutanota.com>
2026-05-08 17:24:16 +0200

    processor.js, rules.html: automaticaly generate rules navigation buttons based on rules.txt

--- js/processor.js
+++ js/processor.js
@@ -1,3 +1,65 @@
+function rulesButtonsToHtml(input) {
+  let i = 0;
+
+  const nextToken = () => {
+    while (i < input.length && /\s/.test(input[i])) i++;
+
+    const start = i;
+
+    while (i < input.length && !/\s/.test(input[i])) i++;
+
+    return input.slice(start, i);
+  };
+
+  const escapeAttr = s =>
+    s.replaceAll("&", "&amp;")
+     .replaceAll('"', "&quot;")
+     .replaceAll("'", "&#39;")
+     .replaceAll("<", "&lt;")
+     .replaceAll(">", "&gt;");
+
+  let str = nextToken();
+  let html = "";
+
+  let header = 1;
+  let phase = 0;
+  let charger = 0;
+
+  while (str && i < input.length) {
+    const buffer = input[i++];
+
+    if (phase === 0 && header === 1) {
+      const safe = escapeAttr(str);
+      html += `<a href="#${safe}"><input type="button" value="${safe}"/></a>\n`;
+      header = 2;
+    }
+
+    if (header === 2 && buffer === "\n") {
+      header = 0;
+    }
+
+    if (buffer === "\n") {
+      charger++;
+
+      if (charger === 2) {
+        phase++;
+
+        if (phase !== 1) {
+          phase = 0;
+          header = 1;
+          str = nextToken();
+          charger = 0;
+        }
+      }
+    } else {
+      charger = 0;
+    }
+  }
+
+  return html;
+}
+
+
 function rulesToHtml(input) {
   let i = 0;
   const nextToken = () => {
